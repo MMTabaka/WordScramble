9c3a4894911ac3cfc9663d1630728e31d21282ee
MMTabaka <miloszmtabaka@gmail.com>
2022-06-26 10:54:15 +0100

    Working with strings

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -12,12 +12,14 @@ struct ContentView: View {
         Text("")
     }
     
-    func loadFile() {
-        if let fileURL = Bundle.main.url(forResource: "some-file", withExtension: "txt") {
-            if let fileContents = try? String(contentsOf: fileURL) {
-                fileContents
-            }
-        }
+    func test() {
+        let word = "swift"
+        let checker = UITextChecker()
+        
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        let allGood = misspelledRange.location = NSNotFound
     }
 }
 
