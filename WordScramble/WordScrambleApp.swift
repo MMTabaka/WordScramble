316d72b8043117b784826f77044c763e4a95d292
MMTabaka <miloszmtabaka@gmail.com>
2022-06-26 10:27:05 +0100

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Milosz Tabaka on 26/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
